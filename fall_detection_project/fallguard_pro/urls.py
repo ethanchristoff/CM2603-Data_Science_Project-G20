c5106992bca2b049a993f413c69a491e102a83b2
ethanchristoff <93596409+ethanchristoff@users.noreply.github.com>
2025-03-03 15:40:29 +0530

    made big changes

--- fall_detection_project/fallguard_pro/urls.py
+++ fall_detection_project/fallguard_pro/urls.py
@@ -1,7 +1,11 @@
-from django.urls import URLPattern, path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
 from . import views
 
 urlpatterns = [
     path('camera_page/',views.get_camera_page, name="camera_page"),
-    path('camera_feed/', views.camera_feed, name='camera_feed'),
+    path('camera_feed/', views.camera_feed, name='camera_feed'),# Debugging page for the camera feed
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
