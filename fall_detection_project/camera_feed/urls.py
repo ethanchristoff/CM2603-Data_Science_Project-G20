a21f96c1e7d0a862902ff4aec338ba2ec911c06f
ethanchristoff <93596409+ethanchristoff@users.noreply.github.com>
2025-03-10 20:49:48 +0530

    Added an alert bar
    News bar now displays alert status for an improved aesthetic and better functionality

--- fall_detection_project/camera_feed/urls.py
+++ fall_detection_project/camera_feed/urls.py
@@ -6,6 +6,7 @@ from . import views
 urlpatterns = [
     path('camera_page/',views.get_camera_page, name="camera_page"),
     path('camera_feed/', views.camera_feed, name='camera_feed'),# Debugging page for the camera feed
+    path('get_fall_status/', views.get_fall_status, name='get_fall_status'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
