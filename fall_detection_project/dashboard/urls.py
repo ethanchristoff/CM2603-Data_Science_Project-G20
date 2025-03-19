78f57f31c172b29473762aa777bf8577ea09fb14
Senuli Wickramage <senuliwickramage@gmail.com>
2025-03-20 00:10:34 +0530

    made some changes

--- fall_detection_project/dashboard/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.dashboard, name='dashboard'),
-    path('api/sensor_data/', views.get_sensor_data, name='get_sensor_data'),
-]
--- fall_detection_project/weekly_report/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.weekly_report, name='weekly-report')
-]
